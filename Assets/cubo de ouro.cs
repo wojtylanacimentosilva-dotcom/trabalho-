bf74c1a88d4220f420014be6b4f6077c3f681613
wojtylanacimentosilva-dotcom <wojtylanacimentosilva@gmail.com>
2026-02-25 11:43:06 -0300

    cubo de ouro em 2D
    kkkkkk

---
+++ Assets/cubo de ouro.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class cubodeouro : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       transform.Translate(1 , 0 , 0); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
