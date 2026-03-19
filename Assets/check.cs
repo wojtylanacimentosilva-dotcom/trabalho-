0aae3971e4046bbb7004ddc9d7743479c15cc613
wojtylanacimentosilva-dotcom <wojtylanacimentosilva@gmail.com>
2026-03-19 11:45:53 -0300

    TRABALHO

--- Assets/check.cs
+++ Assets/check.cs
@@ -2,17 +2,53 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
+    public string mensagem = "Checkpoint ativado!";
+    private float tempo = 0f;
+
+    void Start()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        // garante que é trigger
+        Collider col = GetComponent<Collider>();
+        if (col != null)
         {
-            cubodeouro player = collision.gameObject.GetComponent<cubodeouro>();
+            col.isTrigger = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cubodeouro player = other.GetComponent<cubodeouro>();
 
             if (player != null)
             {
                 player.respawnPosition = transform.position + new Vector3(0, 1, 0);
-                Debug.Log("Checkpoint salvo!");
+
+                Debug.Log(mensagem);
+
+                // ativa mensagem na tela
+                tempo = 2f;
             }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (tempo > 0)
+        {
+            GUIStyle style = new GUIStyle();
+            style.fontSize = 30;
+            style.normal.textColor = Color.white;
+            style.alignment = TextAnchor.MiddleCenter;
+
+            GUI.Label(
+                new Rect(0, Screen.height / 2 - 50, Screen.width, 100),
+                mensagem,
+                style
+            );
+
+            tempo -= Time.deltaTime;
         }
     }
 }
